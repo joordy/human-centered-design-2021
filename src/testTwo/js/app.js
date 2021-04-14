bf810cc0c19f951b891a7b7cdfd4d9819f45dd08
joordy <jmfronik@gmail.com>
2021-04-14 20:20:53 +0200

    Added examples for test week 2

---
+++ src/testTwo/js/app.js
@@ -0,0 +1,29 @@
+let weekOne = document.querySelector('#weekOne');
+let weekTwo = document.querySelector('#weekTwo');
+weekOne.addEventListener('click', (e) => {
+  const conversation = [
+    'Hi Roger! This is your schedule for week 1.',
+    'Training 1. 70 minutes endurance run',
+    'Training 2. 75 minutes fast endurance run',
+    'Training 3. 21 KM slow endurance run',
+  ];
+  playSchedule(conversation);
+});
+
+weekTwo.addEventListener('click', (e) => {
+  const conversation = [
+    'Hi Roger! This is your schedule for week 2.',
+    'Training 1. 95 minutes endurance run',
+    'Training 2. 90 minutes fast endurance run',
+    'Training 3. 30KM slow endurance run, or maximal 3 hours.',
+  ];
+  playSchedule(conversation);
+});
+
+function playSchedule(conversation) {
+  conversation.forEach((convo) => {
+    const utterance = new SpeechSynthesisUtterance(convo);
+    utterance.rate = 0.8;
+    speechSynthesis.speak(utterance);
+  });
+}
